cc756e6cf4861d41294d20bc27e019481a8f06dc
nt0@rawlingscompany.com <nt0@rawlingscompany.com>
2019-10-29 15:51:49 -0400

    Fixed tabs

--- ConvertVMDK.py
+++ ConvertVMDK.py
@@ -1,109 +1,177 @@
 import os
-import sys
-import re
+import sys 
+import re as regexLib
 
-VMID_Start = 461  #This is the VM ID start number
-LoopNum = 1
-state = 0 
-textFileCreate =  "ls *.vmdk | sed  -e 's/.$//' -e 's/.$//' -e 's/.$//' -e 's/.$//' -e 's/.$//' > ConvertRAWfile.txt"
-#textFileCreate =  "ls *.qcow2 | sed  -e 's/.$//' -e 's/.$//' -e 's/.$//' -e 's/.$//' -e 's/.$//' -e 's/.$//' | tee ConvertRAWfile.txt"
-os.system(textFileCreate)
-with open("ConvertRAWfile.txt") as fp:
-    for i, line in enumerate(fp):
-	state = 0
-        if state == 0:
-	    try:
-                print "Current File: "+ line[:-1]
-                expr = re.compile(r".*<rasd:Description>(.+?)</rasd:Description>");
-                out = []
-                with open(line[:-1]+".ovf") as fp2:
-                    for j, line2 in enumerate(fp2):
-                        if expr.findall(line2) != []:
-                            temp = expr.findall(line2)
-                            out.append(temp[0])
-                print " => " + str(out)
+def mainMenu():
+    mmSelect = True
+    while mmSelect:
+        os.system('clear')
+        print ("   ___                                                     ___                          _            ")
+        print ("  / _ \_ __ _____  ___ __ ___   _____  __ /\   /\/\/\     / __\___  _ ____   _____ _ __| |_ ___ _ __ ")
+        print (" / /_)/ '__/ _ \ \/ / '_ ` _ \ / _ \ \/ / \ \ / /    \   / /  / _ \| '_ \ \ / / _ \ '__| __/ _ \ '__|")
+        print ("/ ___/| | | (_) >  <| | | | | | (_) >  <   \ V / /\/\ \ / /__| (_) | | | \ V /  __/ |  | ||  __/ |   ")
+        print ("\/    |_|  \___/_/\_\_| |_| |_|\___/_/\_\   \_/\/    \/ \____/\___/|_| |_|\_/ \___|_|   \__\___|_|   ")
+        print ("v1.0 http://github.com/Ap3x")
+        print ("\n")
+        print ("1-Convert VMDK")
+        print ("2-Convert QCOW2")
+        print ("3-Destroy VM(s)")
+        print ("4-Stop VM(s)")
+        print ("5-Start VM(s)")
+        print ("x-Exit")
 
-                elementNum = 0
-                for data in out:
-                    s = out[elementNum]
+        select = raw_input("Select an option: ")
 
-                    expr2 = re.compile(r'.*SATA');
-                    new = expr2.findall(s)
-                    if new:
-                        print "FOUND SATA"
-	    	        CreateCommand = "qm create "+str(VMID_Start)+ " --name=VulnHub-"+line[:-1]+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --sata0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
-	    	        break
-                    expr3 = re.compile(r'.*IDE');
-                    new3 = expr3.findall(s)
-                    if new3:
-                        print "FOUND IDE"
-	    	        CreateCommand = "qm create "+str(VMID_Start)+ " --name=VulnHub-"+line[:-1]+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --ide0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
-	    	        break
-                    expr4 = re.compile(r'.*SCSI');
-                    new4 = expr4.findall(s)
-                    if new4:
-                        print "FOUND SCSI"
-	    	        CreateCommand = "qm create "+str(VMID_Start)+ " --name=VulnHub-"+line[:-1]+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --scsi0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
-	    	        break
-                    elementNum += 1
-	    except IOError as e:
-	        print "OVF File not found looking for VMX File"
-	        state = 1
-	if state == 1:
-	    try:
-                expr5 = re.compile(r".*ide0:0.present");
-                out = []
-                with open(line[:-1]+".vmx") as fp:
-                    for k, line3 in enumerate(fp):
-                        if expr5.findall(line3) != []:
-                            temp = expr5.findall(line3)
-                            out.append(temp[0])
-			    CreateCommand = "qm create "+str(VMID_Start)+ " --name=VulnHub-"+line[:-1]+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --ide0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
-		expr6 = re.compile(r".*sata0:0.present");
-                out = []
-                with open(line[:-1]+".vmx") as fp:
-                    for k, line4 in enumerate(fp):
-                        if expr6.findall(line4) != []:
-                            temp = expr6.findall(line4)
-                            out.append(temp[0])
-                            CreateCommand = "qm create "+str(VMID_Start)+ " --name=VulnHub-"+line[:-1]+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --sata0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
-                expr7 = re.compile(r".*scsi0:0.present");
-                out = []
-                with open(line[:-1]+".vmx") as fp:
-                    for k, line5 in enumerate(fp):
-                        if expr7.findall(line5) != []:
-                            temp = expr7.findall(line5)
-                            out.append(temp[0])
-                            CreateCommand = "qm create "+str(VMID_Start)+ " --name=VulnHub-"+line[:-1]+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --scsi0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
-#		print " => " + str(out)
-            except IOError as e:
-                print "MISSING VMX or OVF file"
-                sys.exit()
+        if select == "1":
+            ConvertVMDK()
+            ParseConf()
 
-#       CreateCommand = "qm create "+str(VMID_Start)+ " --name=VulnHub-"+line[:-1]+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --sata0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
-#	CreateCommand = "qm create "+str(VMID_Start)+ " --name=VulnHub-"+line[:-1]+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --ide0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
-        os.system(CreateCommand)
-        print "Created VM " + str(VMID_Start) +" called VulnHub-"+line[:-1]
-        VMID_Start += 1
-	LoopNum += 1
-print "Done: Created "+str(LoopNum-1)+" VMs"
+        elif select == "2":
+            ConvertQCOW2()
+            ParseConf()
+        #elif select == "3":
+        #
+        #elif select == "4":
+        #
+        #elif select == "5":
+
+def ConvertVMDK():
+    path = raw_input("Path of folder with VMDK file(s): ")
+    os.system("cd " + path)
+    getFileName =  "ls *.vmdk | sed  -e 's/.$//' -e 's/.$//' -e 's/.$//' -e 's/.$//' -e 's/.$//' > ConvertRAWfile.txt"	#List the files then removes the last 5 chars then puts each line in ConvertRAWfile
+    os.system(getFileName)
+    try:
+        with open("ConvertRAWfile.txt") as fp:
+            for i, line in enumerate(fp):
+                SearchForOVF(line)
+                SearchForVMX(line)
+    except IOError as e:
+        print ("MISSING VMX or OVF file")
+        sys.exit()
+
+def ConvertQCOW2():
+    path = raw_input("Path of folder with QCOW2 file(s): ")
+    os.system("cd " + path)
+    getFileName =  "ls *.qcow2 | sed  -e 's/.$//' -e 's/.$//' -e 's/.$//' -e 's/.$//' -e 's/.$//' -e 's/.$//' | tee ConvertRAWfile.txt" #List the files then removes the last 6 chars then puts each line in ConvertRAWfile
+    os.system(getFileName)
+    try:
+        with open("ConvertRAWfile.txt") as fp:
+            for i, line in enumerate(fp):
+                SearchForOVF(line)
+                SearchForVMX(line)
+    except IOError as e:
+        print ("MISSING VMX or OVF file")
+        sys.exit()
+
+def SearchForOVF(fileName):
+    try:
+        print("Current File: "+ fileName)				            					# Read the first line of ConvertRAWfile.txt
+        expression = regexLib.compile(r".*<rasd:Description>(.+?)</rasd:Description>");	# Expression to search for
+        out = []
+        with open(fileName+".ovf") as enumFile:						            		# Search each line of the .ovf file IF the file is not found then the error is caught and searches a vmx file
+            for lineNum, lineInfo in enumerate(enumFile):					            # For each line in ovf file
+                if expression.findall(lineInfo) != []:							        # Read the line looking for the expression
+                    temp = expression.findall(lineInfo)							        # If found between Description tags in ovf add to temp
+                    out.append(temp[0])										            # temp is added to array
+        print (" => " + str(out))
+    except IOError as e:
+       print("OVF File not found looking for VMX File")
+
+def SearchForVMX(fileName):
+        expr = regexLib.compile(r".*ide0:0.present")
+        out = []
+        with open(fileName+".vmx") as enumFile:
+            for lineNum, lineInfo in enumerate(enumFile):
+                if expr.findall(lineInfo) != []:
+                    temp = expr.findall(lineInfo)
+                    out.append(temp[0])
+                    CreateVMfromVMX("ide")
+
+        expr = re.compile(r".*sata0:0.present")
+        out = []
+        with open(fileName+".vmx") as enumFile:
+            for lineNum, lineInfo in enumerate(enumFile):
+                if expr.findall(lineInfo) != []:
+                    temp = expr.findall(lineInfo)
+                    out.append(temp[0])
+                    CreateVMfromVMXby("sata")
+
+        expr = regexLib.compile(r".*scsi0:0.present")
+        out = []
+        with open(fileName+".vmx") as enumFile:
+            for lineNum, lineInfo in enumerate(enumFile):
+                if expr.findall(lineInfo) != []:
+                    temp = expr.findall(lineInfo)
+                    out.append(temp[0])
+                    CreateVMfromVMX("scsi")
+
+def CreateVMfromVMXbyIDE(vmID, vmName, typeOfHD):
+    createCommand = "qm create "+str(vmID)+ " --name="+vmName+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --"+typeOfHD+"0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
+    return createCommand
+
+def CreateVMfromOVF(vmID, vmName, typeOfHD):
+    createCommand = "qm create "+str(vmID)+ " --name="+vmName+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --"+typeOfHD+"0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
+
+def CreateVM(command, vmID, vmName ):
+    os.system(command)
+    print("Created VM " + str(vmId) +" called "+vmName)
+
+def ParseOVF():
+    VMID_Start = 461  #This is the VM ID start number
+    LoopNum = 1
+    with open("ConvertRAWfile.txt") as enumFile:
+        for i, line in enumerate(enumFile):
+            if state == 0:
+                try: 
+                    SearchOVF()
+                    elementNum = 0
+                    for data in out:
+                        s = out[elementNum]
+                        expr2 = regexLib.compile(r'.*SATA')
+                        expr3 = regexLib.compile(r'.*IDE')
+                        expr4 = regexLib.compile(r'.*SCSI')
+
+                        new = expr2.findall(s)
+                        if new:
+                            CreateVMfromOVF("sata")
+                            break
+
+                        new3 = expr3.findall(s)
+                        if new3:
+                            CreateVMfromOVF("ide")
+                            break
+
+                        new4 = expr4.findall(s)
+                        if new4:
+                            CreateVMfromOVF("scsi")
+                            break
+                        elementNum += 1
+                except IOError as e:
+                    print("OVF File not found looking for VMX File")
+#   createCommand = "qm create "+str(VMID_Start)+ " --name=VulnHub-"+line[:-1]+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --sata0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
+#	createCommand = "qm create "+str(VMID_Start)+ " --name=VulnHub-"+line[:-1]+" --onboot=1 --ide2=none,media=cdrom --ostype=l26 --scsihw=virtio-scsi-pci --ide0=Backup:1,format=qcow2 --sockets=1 --cores=1 --numa=0 --memory=512 --net0=virtio,bridge=vmbr1"
+#CreateVM()
+#print("Created VM " + str(VMID_Start) +" called VulnHub-"+line[:-1])
+#VMID_Start += 1
+#LoopNum += 1
+#print("Done: Created "+str(LoopNum-1)+" VMs")
+#
+#VMID_Start -= LoopNum
+#VMID_Start += 1
 
 
-VMID_Start -= LoopNum
-VMID_Start += 1
-
-with open("ConvertRAWfile.txt") as fp:
-    for i, line in enumerate(fp):
+with open("ConvertRAWfile.txt") as enumFile:
+    for i, line in enumerate(enumFile):
        cmdConvert = "qemu-img convert -f vmdk " +line[:-1]+ ".vmdk -O qcow2 " +line[:-1]+ ".qcow2"
        os.system(cmdConvert)
-       print "Successful conversion of " + line[:-1]+ ".vmdk to " + line[:-1]+ ".qcow2"
-       cmdMove = "mv " +line[:-1]+ ".qcow2 /Backup/images/"+str(VMID_Start)+"/vm-"+str(VMID_Start)+"-disk-1.qcow2"
+       print("Successful conversion of " + line[:-1]+ ".vmdk to " + line[:-1]+ ".qcow2")
+       cmdMove = ("mv " +line[:-1]+ ".qcow2 /Backup/images/"+str(VMID_Start)+"/vm-"+str(VMID_Start)+"-disk-1.qcow2")
        os.system(cmdMove)
-       print "Moved "+line[:-1]+ ".qcow2 to  /Backup/images/"+str(VMID_Start)+"/vm-"+str(VMID_Start)+"-disk-1.qcow2"
+       print ("Moved "+line[:-1]+ ".qcow2 to  /Backup/images/"+str(VMID_Start)+"/vm-"+str(VMID_Start)+"-disk-1.qcow2")
 #       os.system("rm -rf "+ line[:-1]+ ".raw")
 #       print "Deleted "+ line[:-1]+ ".raw"
        VMID_Start += 1 
-       print "Completed"
+       print ("Completed")
 
-print "Converted and moved all the files"
+print ("Converted and moved all the files")
 os.system("rm -rf ConvertRAWfile.txt")
